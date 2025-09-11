660c4774e4f2e1de026825f16afd4d588610842c
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-09-11 21:25:18 +0300

    feat:add increase and decrease controls to Counter component
    Enhanced the Counter component to support both incrementing and decrementing the count by selectable amounts.
    
    Updated the UI layout and styles in App.css to better organize the controls and improve appearance.

--- classworks/11.09/app/src/components/Counter.tsx
+++ classworks/11.09/app/src/components/Counter.tsx
@@ -7,12 +7,35 @@ export default function Counter() {
     setCount((count) => count + amount);
   }
 
+  function decreaseCounter(amount: number) {
+    setCount((count) => count - amount);
+  }
+
   return (
     <>
       <h1>My App</h1>
-      <p>Try Button!</p>
-      <div className="card">
-        <button onClick={() => increaseCounter(5)}>Count is {count}</button>
+      <div>Number is: 
+        <h2>{count}</h2>
+      </div>
+
+      <div>
+        <div className="card">
+          Increase By (+):
+          <button onClick={() => increaseCounter(1)}>1</button>
+          <button onClick={() => increaseCounter(25)}>25</button>
+          <button onClick={() => increaseCounter(50)}>50</button>
+          <button onClick={() => increaseCounter(100)}>100</button>
+        </div>
+      </div>
+
+      <div>
+        <div className="card">
+          Descrease By (-):
+          <button onClick={() => decreaseCounter(1)}>1</button>
+          <button onClick={() => decreaseCounter(25)}>25</button>
+          <button onClick={() => decreaseCounter(50)}>50</button>
+          <button onClick={() => decreaseCounter(100)}>100</button>
+        </div>
       </div>
     </>
   );
