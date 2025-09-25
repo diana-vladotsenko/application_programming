e0636fff8971210cfdb102c48a3c415dc388a805
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-09-25 22:13:21 +0300

    feat (homeworks): add homework task 3 examples for Prop drilling vs Context
    - added Context.tsx demonstrating React Context
    - added PropDrilling.tsx demonstrating prop drilling

--- classworks/2025-09-25/diana/frontend/src/App.tsx
+++ classworks/2025-09-25/diana/frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Cats from "./components/Cats";
+import Context from "./forme/Context";
+import PropDrilling from "./forme/PropDrilling";
 
 function App() {
   return (
     <>
       <Cats />
+      <PropDrilling/>
+      <Context/>
     </>
   );
 }
